49eebe37b40b1c319aedd124076ae00b92856d87
fikribijikurma <auliyafikrirahmadani@gmail.com>
2026-05-18 11:02:11 +0700

    Membuat kelas entitas User dengan anotasi JPA dan Lombok (@Data, @Builder) dan Menghubungkan entitas dengan tabel 'users'

--- src/main/java/com/deploy/pertemuan11/model/User.java
+++ src/main/java/com/deploy/pertemuan11/model/User.java
@@ -1,4 +1,13 @@
 package com.deploy.pertemuan11.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@Builder
+@Table(name = "users")
 public class User {
 }
